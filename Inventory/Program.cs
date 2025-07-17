0a5a9d1fae0349efa1e633f6787e61a9841a8e45
Sylwester Sowula <sylwester.sowula@gmail.com>
2025-07-17 18:55:52 +0200

    add method for update products

--- Inventory/Program.cs
+++ Inventory/Program.cs
@@ -32,7 +32,7 @@
         Quantity.Add(quantity);
         Console.WriteLine("Succsess");
     }
-     public static void showAllProducts()
+    public static void showAllProducts()
     {
         int len = Products.Count;
         if (len == 0)
@@ -46,6 +46,85 @@
             {
                 Console.WriteLine($"{i + 1}. {Products[i]} - {Price[i]} - {Quantity[i]}");
             }
+        }
+    }
+     public static void updateProducts()
+    {
+        int len = Products.Count;
+        Console.WriteLine("Enter an Id of product you want to update: ");
+        int id;
+        if (!int.TryParse(Console.ReadLine(), out id))
+        {
+            Console.WriteLine("You need to enter an intiger");
+            return;
+        }
+        if (id > len + 1)
+        {
+            Console.WriteLine("We dont have that many products");
+            return;
+        }
+        else if (id <= 0)
+        {
+            Console.WriteLine("Id must be highier than 0");
+            return;
+        }
+        Console.WriteLine("What do you want to update?");
+        Console.WriteLine("1. Name\n2. Price \n3. Quantity");
+        int pick;
+        if (!int.TryParse(Console.ReadLine(), out pick))
+        {
+            Console.WriteLine("You need to enter an intiger");
+            return;
+        }
+        if (pick == 1)
+        {
+            Console.WriteLine("Please enter new name for this product");
+            string? newName = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(newName))
+            {
+                Console.WriteLine("Wrong name");
+                return;
+            }
+            else
+            {
+                Products[id - 1] = newName;
+                Console.WriteLine("Succsess");
+            }
+        }
+        else if (pick == 2)
+        {
+            Console.WriteLine("Please enter new price for this product");
+            double newPrice;
+            if (!double.TryParse(Console.ReadLine(), out newPrice))
+            {
+                Console.WriteLine("Wrong price");
+                return;
+            }
+            else
+            {
+                Price[id - 1] = newPrice;
+                Console.WriteLine("Succsess");
+            }
+        }
+        else if (pick == 3)
+        {
+            Console.WriteLine("Please enter new quantity for this product");
+            int newQuantity;
+            if (!int.TryParse(Console.ReadLine(), out newQuantity))
+            {
+                Console.WriteLine("Wrong quantity");
+                return;
+            }
+            else
+            {
+                Quantity[id - 1] = newQuantity;
+                Console.WriteLine("Succsess");
+            }
+        }
+        else
+        {
+            Console.WriteLine("There is no option with this number");
+            return;
         }
     }
 }
