a4453dee8d82477b59f9db04e1c19d9a737cae14
Sylwester Sowula <sylwester.sowula@gmail.com>
2025-07-17 18:49:21 +0200

    add lists for data storage

--- Inventory/Program.cs
+++ Inventory/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿class Inventory
+{
+    public static List<string> Products = new List<string>();
+    public static List<double> Price = new List<double>();
+    public static List<int> Quantity = new List<int>();
+    int len = Products.Count;
+}
